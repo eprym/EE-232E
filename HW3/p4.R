a3d48197e2c304a07cef4256bdc47dfd17f9b118
yuliqiang <1198693608@qq.com>
2016-05-01 21:35:59 -0700

    update report

--- HW3/p4.R
+++ HW3/p4.R
@@ -1,6 +1,6 @@
 library("igraph")
 g <- read.graph("sorted_directed_net.txt", format="ncol", directed=TRUE)
-cl <- clusters(g, mode="strong")
+cl <- clusters(g)
 gcc_idx = which.max(cl$csize)
 non_gcc_idx = (1:vcount(g))[cl$membership != gcc_idx]
 gcc = delete.vertices(g, non_gcc_idx)
@@ -12,6 +12,7 @@ max_idx <-  which.max(sizes(fgc))
 non_max_idx <-  (1:vcount(gcc_ud))[fgc$membership != max_idx]
 maxFastCom <- delete.vertices(gcc_ud, non_max_idx)
 maxSubCom <- fastgreedy.community(maxFastCom)
+sizes(maxSubCom)
 modularity(maxSubCom)
 png(filename="large_sub_com.png")
 plot(maxSubCom, maxFastCom, vertex.label=NA)
--- HW3/p5.R
+++ HW3/p5.R
@@ -1,18 +1,22 @@
 library("igraph")
 g <- read.graph("sorted_directed_net.txt", format="ncol", directed=TRUE)
-cl <- clusters(g, mode="strong")
+cl <- clusters(g)
 gcc_idx = which.max(cl$csize)
 non_gcc_idx = (1:vcount(g))[cl$membership != gcc_idx]
 gcc = delete.vertices(g, non_gcc_idx)
 gcc_ud <-  as.undirected(gcc, mode="collapse", edge.attr.comb = list(weight="prod"))
 E(gcc_ud)$weight <- sqrt(E(gcc_ud)$weight)
 fgc <-  fastgreedy.community(gcc_ud)
+max_idx <-  which.max(sizes(fgc))
+non_max_idx <-  (1:vcount(gcc_ud))[fgc$membership != max_idx]
+maxFastCom <- delete.vertices(gcc_ud, non_max_idx)
+maxSubCom <- fastgreedy.community(maxFastCom)
 
-over_100_idx <-  which(sizes(fgc)>100)
+over_100_idx <-  which(sizes(maxSubCom)>100)
 mod <-  rep(0, length(over_100_idx))
 for(i in 1:length(over_100_idx)){
-  non_idx <-  (1:vcount(gcc_ud))[fgc$membership != over_100_idx[i]]
-  fastCom <- delete.vertices(gcc_ud, non_idx)
+  non_idx <-  (1:vcount(maxFastCom))[maxSubCom$membership != over_100_idx[i]]
+  fastCom <- delete.vertices(maxFastCom, non_idx)
   subCom <- fastgreedy.community(fastCom)
   mod[i] <- modularity(subCom)
   imgname = sprintf("sub_com_%d.png", i)
